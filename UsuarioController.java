cb1782851c7c206ed57d1ce5bd640ce3087174cb
Joaquín Gomeza <93411378+Joucomotox@users.noreply.github.com>
2022-11-07 19:09:34 -0300

    Update UsuarioController.java

--- UsuarioController.java
+++ UsuarioController.java
@@ -2,5 +2,14 @@ import java.util.List;
 
 public class UsuarioController {
     List <Usuario> ListaUsuarios;
+
+    public String ConsultarPeticiones(dniUsuario, dniPaciente){
+        if (Peticion.paciente.dni == dniPaciente)
+            return 
+    }
+
+    public void ModificarPaciente(dniUsuario, dniPaciente){
+        Paciente.dni = dniPaciente;
+    }
 }
 
