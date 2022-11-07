ebf1a62fae2f307215cdffe8620a5649d6b0699e
Maxmart27 <Maximmart007@gmail.com>
2022-11-07 19:19:35 -0300

    Pequenio cambio PacienteController

--- PacienteController.java
+++ PacienteController.java
@@ -4,8 +4,8 @@ public class PacienteController {
     List <Paciente> listaPacientes;
     public String eliminarPaciente(int dniPaciente){
         for (int i = 0; i>listaPacientes.size(); i++){
-            if (dniPaciente == listaPacientes.get(i).getdni()){
-
+            if (dniPaciente == listaPacientes.get(i).getDni()){
+                
             }
         }
     }
