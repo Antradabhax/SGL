bcf82039071878ef8cc8660b37ca19fc0871187f
Joaquín Gomeza <93411378+Joucomotox@users.noreply.github.com>
2022-11-07 18:40:36 -0300

    Update ResultadoPractica.java

--- ResultadoPractica.java
+++ ResultadoPractica.java
@@ -1,16 +1,3 @@
 public class ResultadoPractica {
     private tipoReserva confidencialidad;
 }
-
-public class ResultadoPracticaCritico extends ResultadoPractica{
-    private float resultado;
-}
-
-public class ResultadoPracticaBooleano extends ResultadoPractica{
-    private boolean valorResultado;
-}
-
-public class ResultadoPracticaCadena extends ResultadoPractica{
-    private String valorResultado;
-    private boolean  mostrarValorCritico;
-}
